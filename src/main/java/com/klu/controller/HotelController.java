84858c4d3c96cfb1968a16c897b6338767c64fee
V V PAVITRA HANUMASRI <163735552+pavitravedulla@users.noreply.github.com>
2025-09-20 10:45:24 +0530

    Update HotelController.java

--- src/main/java/com/klu/controller/HotelController.java
+++ src/main/java/com/klu/controller/HotelController.java
@@ -1,1 +1,29 @@
 
+package com.klu.controller;
+
+import com.klu.model.Hotel;
+import com.klu.service.HotelService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/back3")
+public class HotelController {
+
+    private final HotelService hotelService;
+
+    public HotelController(HotelService hotelService) {
+        this.hotelService = hotelService;
+    }
+
+    @GetMapping("/list")
+    public List<Hotel> getHotels() {
+        return hotelService.getAllHotels();
+    }
+
+    @PostMapping("/add")
+    public Hotel addHotel(@RequestBody Hotel hotel) {
+        return hotelService.addHotel(hotel);
+    }
+}
