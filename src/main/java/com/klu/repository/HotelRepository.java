88ae0107e498fe1c63b9ebd18fd88726902857c5
V V PAVITRA HANUMASRI <163735552+pavitravedulla@users.noreply.github.com>
2025-09-20 10:53:31 +0530

    Update HotelRepository.java

--- src/main/java/com/klu/repository/HotelRepository.java
+++ src/main/java/com/klu/repository/HotelRepository.java
@@ -1,7 +1,9 @@
 package com.klu.repository;
 
-
-import com.klu.model.Booking;
+import com.klu.model.Hotel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface HotelRepository extends JpaRepository<Booking, Long> {}
+@Repository
+public interface HotelRepository extends JpaRepository<Hotel, Long> {
+}
