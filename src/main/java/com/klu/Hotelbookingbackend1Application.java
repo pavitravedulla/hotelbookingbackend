8c260d600cff372d8942ebffb806da8d2f4b2fa5
V V PAVITRA HANUMASRI <163735552+pavitravedulla@users.noreply.github.com>
2025-09-20 07:51:17 +0530

    Update Hotelbookingbackend1Application.java

--- src/main/java/com/klu/Hotelbookingbackend1Application.java
+++ src/main/java/com/klu/Hotelbookingbackend1Application.java
@@ -2,12 +2,19 @@ package com.klu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Hotelbookingbackend1Application {
+public class Hotelbookingbackend1Application extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Hotelbookingbackend1Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Hotelbookingbackend1Application.class, args);
+    }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        // Point to your main application class
+        return application.sources(Hotelbookingbackend1Application.class);
+    }
 }
