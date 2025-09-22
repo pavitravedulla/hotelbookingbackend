1c091fbaa9289e302f809799186d5d215bbe339b
V V PAVITRA HANUMASRI <163735552+pavitravedulla@users.noreply.github.com>
2025-09-22 14:42:59 +0530

    Update Hotel.java

--- src/main/java/com/klu/model/Hotel.java
+++ src/main/java/com/klu/model/Hotel.java
@@ -1,11 +1,13 @@
 package com.klu.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
+@Table(name = "hotel")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Hotel {
 
     @Id
@@ -13,28 +15,8 @@ public class Hotel {
     private Long id;
 
     private String name;
+
     private String location;
-    private int rating;
 
-    public Hotel() {}
-
-    public Hotel(String name, String location, int rating) {
-        this.name = name;
-        this.location = location;
-        this.rating = rating;
-    }
-
-    // Getters and Setters
-
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-
-    public String getLocation() { return location; }
-    public void setLocation(String location) { this.location = location; }
-
-    public int getRating() { return rating; }
-    public void setRating(int rating) { this.rating = rating; }
+    private Double rating;
 }
